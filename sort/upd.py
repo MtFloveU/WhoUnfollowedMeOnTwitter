9c065c767e455ce2846b74574b0ad3336778abaa
Poempoat <liujiayou2008@163.com>
2024-12-22 10:22:59 +0800

    fix update data wouldn't modify single unfollow

--- sort/upd.py
+++ sort/upd.py
@@ -9,7 +9,7 @@ removed_list_path = "./data/removed_list.txt"
 def json_data(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
-        return {key: data[key] for key in ('screen_name', 'name', 'description')}
+        return {key: data[key] for key in ('screen_name', 'name', 'description', 'followed_by', 'following')}
 
 with open(removed_list_path, 'r') as removed_list_file:
     removed_list = removed_list_file.read().splitlines()
@@ -23,7 +23,7 @@ for filename in os.listdir(source_dir):
             source_data = json_data(source_file_path)
             with open(target_file_path, 'r') as target_file:
                 target_data = json.load(target_file)
-            target_data = {key: target_data[key] for key in ('screen_name', 'name', 'description')}
+            target_data = {key: target_data[key] for key in ('screen_name', 'name', 'description', 'followed_by', 'following')}
 
             if source_data != target_data:
                 shutil.copy2(source_file_path, target_file_path)
